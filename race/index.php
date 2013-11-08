4e3e03ac42ddab93a243ec13ca32f2bb3ba3e82d
luiz.dapieve <luiz.dapieve@vs-desktop.(none)>
2013-11-08 16:51:25 -0200

    Adicionando mais testes

--- race/index.php
+++ race/index.php
@@ -2,11 +2,15 @@
 include_once 'bootstrap.php';
 
 $carRacer1 = new Car();
-$carRacer1->setEngine(new Engine(205), 'Saveiro 1.6');
+$carRacer1->setEngine(new Engine(205));
+$carRacer1->setModel('Saveiro 1.6');
+
 $racer1 = new Racer('Luiz', $carRacer1);
 
 $carRacer2 = new Car();
-$carRacer2->setEngine(new Engine(230), 'Maverick');
+$carRacer2->setEngine(new Engine(230));
+$carRacer2->setModel('Maverick');
+
 $racer2 = new Racer('Vin Diesel', $carRacer2);
 
 $race = new Race();
@@ -15,12 +19,15 @@ $race->setRacers($racer1, $racer2);
 $quantityRacers = count($race->getRacers());
 
 $street = new Street(1000);
-
 $race->addTrack($street);
+
 echo "==================================================================================================================== \n";
 echo "| A disputa é entre ".$quantityRacers . " corredores. Em uma pista de ".$street->getDistance()." m.  \n";
 echo "| O corredor ".$racer1->getName()." está com um(a) ". $carRacer1->getModel()." que tem de final ".$carRacer1->getTopSpeed(). "km/h. \n";
 echo "| O corredor ".$racer2->getName()." está com um(a) ". $carRacer2->getModel()." que tem de final ".$carRacer2->getTopSpeed(). "km/h. \n";
 echo "==================================================================================================================== \n";
 
-$race->startRace();
+$result = $race->startRace($racer1,$racer2,$street->getDistance());
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% \n";
+echo $result. "\n";
+echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% \n";
